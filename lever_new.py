b577a8f4f0431c524e32d2088458d2f397b34d31
talibhmukadam <talibhmukadam@gmail.com>
2025-05-29 22:34:22 +0700

    Update lever_new.py

--- lever_new.py
+++ lever_new.py
@@ -31,7 +31,7 @@ with SB(uc=True, test=True, locale="en") as sb:
     print ("resume uploaded successfully")
     #company
     sb.cdp.find_element('[name="org"]', best_match=False, timeout=10).send_keys(j.get("user",{}).get("resumeData",{}).get("workExperience",[])[0].get("company","") or "NA") 
-    
+    print ("company filled")
     #social links
     sb.cdp.find_element('[name="urls[LinkedIn]"]', best_match=False, timeout=10).send_keys(j.get("user",{}).get("personalDetails",{}).get("linkedin","") or "NA") 
     sb.cdp.find_element('[name="urls[Portfolio]"]', best_match=False, timeout=10).send_keys(j.get("user",{}).get("personalDetails",{}).get("portfolio","NA") or "NA" ) 
